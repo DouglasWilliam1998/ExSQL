108ad6bcf94d1eb7d215245d3c39e65a7caaa520
DouglasWilliam1998 <douglaswilliam1998@gmail.com>
2022-12-02 20:21:26 -0300

    Teste

---
+++ ex28-Quanto Ganha um Médico.sql
@@ -0,0 +1,8 @@
+SELECT
+doctors.name,
+ROUND(SUM((attendances.hours * 150)+((attendances.hours * 15)*work_shifts.bonus)*.1), 1) AS salary
+FROM doctors, attendances, work_shifts
+WHERE doctors.id=attendances.id_doctor
+AND work_shifts.id=attendances.id_work_shift
+GROUP BY doctors.name
+ORDER BY salary DESC;
